9e36cb3e92bf0b5d0366e853970c0f6d96baf2e7
Nabaraj <>
2023-02-17 19:10:58 +0545

    update

--- app.js
+++ app.js
@@ -1,51 +1,71 @@
 const input = document.querySelector("input");
 const card = document.querySelector(".card");
 const button = document.querySelector("#button");
-const section = document.getElementById("section");
+const cards = document.getElementById("card");
+const id2 = document.getElementById("2");
 
 window.onload = () => {
-  let datos = JSON.parse(window.localStorage.getItem("datas"));
-  search(datos);
+  cards.classList.add("d-none");
+  id2.classList.add("d-none");
 };
+
 const getValue = () => {
   return input.value;
 };
-const search = async datos => {
-  let h5 = (document.createElement("h5").innerText = "loading...");
-  card.innerHTML = h5;
-  let value = getValue();
+const searchBook = () => {
+  let query = getValue();
+  query &&
+    setTimeout(async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=12`
+        );
+        const datas = await response.json();
+        console.log("data", datas.items);
+        displayData(datas.items);
+      } catch (err) {
+        console.log("err", err);
+      }
+    }, 500);
+};
+const removeBook = data => {
+  data
+    ? document.getElementById("book").classList.add("d-none")
+    : document.getElementById("book").classList.remove("d-none");
+};
+const displayData = data => {
+  console.log("data", data);
   let html = "";
-  try {
-    const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${
-        value == "" ? "2" : value
-      }&maxResults=12`
-    );
-    const data = await response.json();
-    window.localStorage.setItem("datas", JSON.stringify(data));
-    data
-      ? document.getElementById("book").classList.add("d-none")
-      : document.getElementById("book").classList.remove("d-none");
-    let datas = data ? data : datos;
-    console.log("datas", datas);
-    datas.items.map((data, index) => {
+  removeBook(data);
+  cards.classList.remove("d-none");
+  setTimeout(() => {
+    cards.classList.add("d-none");
+    data.map((data, index) => {
+      const image = data.volumeInfo.imageLinks.thumbnail
+        ? data.volumeInfo.imageLinks.thumbnail
+        : "https://via.placeholder.com/128x196?text=No+Image";
+      const title = data.volumeInfo.title
+        ? data.volumeInfo.title.slice(0, 34)
+        : "Unknown";
+      const date = data.volumeInfo.publishedDate
+        ? data.volumeInfo.publishedDate
+        : "Unknown";
+      const publisher = data.volumeInfo.publisher
+        ? data.volumeInfo.publisher
+        : "Unknown";
       html += `
               <section class="card-main">
               <div class="card-image">
               <figure>
-              <img src=${data.volumeInfo.imageLinks.thumbnail} />
+              <img src=${image} />
               </figure>
               </div>
               <div class="card-description">
-              <h3>${data.volumeInfo.title.slice(0, 34) ?? ""}</h3>
-              <h3>Published Date : <span>${
-                data.volumeInfo.publishedDate ?? ""
-              }</span></h3>
-              <h3>Publisher : <span>${
-                data.volumeInfo.publisher ?? ""
-              }</span></h3>
+              <h3>${title}</h3>
+              <h3>Published Date : <span>${date}</span></h3>
+              <h3>Publisher : <span>${publisher}</span></h3>
               <div>
-              <a href="./search.html" class="search card-search">
+              <a class="search card-search" onclick="detail(title)">
               Moreinfo
               <i
                 class="fa fa-long-arrow-right"
@@ -59,14 +79,16 @@ const search = async datos => {
           `;
     });
     card.innerHTML = html;
-  } catch (err) {
-    console.log("error", err);
-  }
+  }, 500);
 };
 
-button.addEventListener("click", search);
+function detail(pub) {
+  search();
+  console.log("url", pub);
+}
+input.addEventListener("keyup", searchBook);
 input.addEventListener("keypress", e => {
   if (e.key === "Enter") {
-    search();
+    searchBook();
   }
 });
